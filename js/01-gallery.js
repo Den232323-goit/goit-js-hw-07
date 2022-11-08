9c88f91f4dcb294cb6c99a9e55798b8ec1904f78
Denis Kovalsky <100243898+Den232323-goit@users.noreply.github.com>
2022-11-08 21:28:43 +0200

    111

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,37 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector(".gallery");
 
-gallery.insertAdjacentHTML(
-  `afterbegin`,
-  galleryItems
-    .map(
-      (img) =>
-        `<a href="${img.original}">
-	  <img
-	  src="${img.preview}" 
-	  alt="${img.description}">
-	  </a>`
-    )
-    .join("")
-);
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+galleryRef.insertAdjacentHTML(`afterbegin`, galleryMarkup);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href='${original}'>
+		<img class="gallery__image"
+		scr='${preview}'
+		data-source='${original}'
+		alt='${description}'/>
+		</a>`;
+    })
+    .join("");
+}
+
+galleryRef.addEventListener("click", onGalleryRefClick);
+
+let modalWindow;
+
+function onGalleryRefClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  modalWindow = basicLightbox.create(
+    `<img scr='${event.target.dataset.source}' width="800" height="600">`
+  );
+
+  modalWindow.show();
+}
